0484fbc3672430ff76feee1d6663acf7ec555c71
Bill Li <billmli88@gmail.com>
2021-10-08 00:16:10 -0700

    stats handling other user arguments

--- commands/stats.ts
+++ commands/stats.ts
@@ -3,14 +3,20 @@ import {playerMap} from "../index"
 (module).exports = {
     name: 'stats',
     description: "returns stats of current car",
-    
     execute(message :any , args:any){
-        var id = message!.member!.id!;
-        console.log(id);
+        let isUsers = true;
+        if(args.length === 2){
+            var id = message!.member!.id!;
+        }
+        else if(args.length > 2 && args[2]!.length > 10){
+            isUsers = false;
+            id = args[2]!.substring(2,args[2].length-2);
+        }
 
+        console.log(id);
         console.log(args);
         if(playerMap.get(id) == null || playerMap.get(id).getCurrentCar == null){
-            message.channel.send("You do not have a car selected")
+            message.channel.send(((isUsers) ? "You do":"User does") + " not have a car selected")
         }
         else{
             let user = playerMap.get(id);
